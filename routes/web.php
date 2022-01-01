5df768209b553f0adb8b9be12e778fe13da8e727
Satish Kumar Sharma <skwebserv@gmail.com>
2022-01-01 11:54:50 +0530

    fixed 404 on refresh page except home page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::view('/{path?}', 'welcome');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
